0a28567aaa5a36813c44e65a1b607956a1c62f6c
JadenBali <ssjmjb123@gmail.com>
2025-07-22 15:34:41 -0700

    Version 3

--- flat/flat.js
+++ flat/flat.js
@@ -10,6 +10,8 @@ selectorObject.style.backgroundColor = `yellow`
 selectorItems.style.display = `flex`
 selectorItems.style.gap = `10px`
 
+
+
 selectorObject2.style.height = `100px`
 selectorObject2.style.width = `100px`
 selectorObject2.style.backgroundColor = `blue`
@@ -21,30 +23,20 @@ selectorOperator.style.backgroundColor = `hsl(120, 100%, 50%)`
 selectorOperator.style.border = `none`
 selectorOperator.style.borderRadius = `10px`
 
-document.addEventListener(`click`, event =>{
-    switch(event.target.textContent){
-        case `turn on selector`:
-            event.target.textContent = `turn of selector`
-            selectorObject.classList.add(`selector`);
-            break;
-        case `turn of selector`:
-            selectorObject.classList.remove(`selector`)
-            selectorObject2.classList.remove(`selector`);
-            event.target.textContent = `turn on selector`
-            sele
-            break;
-    }
+selectorOperator.addEventListener(`click`, event =>{
+    selectorObject.classList.add(`selector`);
 })
 
 document.addEventListener(`keydown`, event =>{
-    if(event.key == `ArrowRight`){
+        if(event.key == `ArrowRight`){
         selectorObject.classList.remove(`selector`);
         selectorObject2.classList.add(`selector`);
     }
-    else if(event.key == `ArrowLeft`){
+        if(event.key == `ArrowLeft`){
         selectorObject2.classList.remove(`selector`)
         selectorObject.classList.add(`selector`);
+        }
+        
     }
-    console.log(event.key)
-})
-
+    
+)
